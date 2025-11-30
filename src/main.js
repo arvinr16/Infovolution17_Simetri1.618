bea914c61b8ac70ba788999154a965184f00fe05
arvinr16 <arviansyahyk@gmail.com>
2025-11-30 12:58:36 +0700

    Adding 'landing' section in 'main.js' (finish)

--- src/main.js
+++ src/main.js
@@ -15,3 +15,40 @@ document.addEventListener('click', (e) => {
         navLink.classList.remove('open');
     }
 });
+
+const texts = ["Welcome to our official company website",
+               "We Design. We Create. We Inspire",
+               "Learn more about who we are."
+];
+
+let x = 0;
+let y = 0;
+let currentText = texts[y];
+let isDeleting = false;
+const speeds = 60;
+const deleteSpeed = 40;
+const delay = 700;
+
+function typewriter() {
+    const element = document.getElementById('typewriter');
+
+    if(!isDeleting && x < currentText.length) {
+        element.innerHTML = currentText.substring(0, x + 1);
+        x++
+        setTimeout(typewriter, speeds);
+    } else if (isDeleting && x > 0) {
+        element.innerHTML = currentText.substring(0, x - 1);
+        x--;
+        setTimeout(typewriter, deleteSpeed);
+    } else if (!isDeleting && x === currentText.length) {
+        isDeleting = true;
+        setTimeout(typewriter, delay);
+    } else if (isDeleting && x === 0) {
+        isDeleting = false;
+        y = (y + 1) % texts.length;
+        currentText = texts[y];
+        setTimeout(typewriter, delay);
+    }
+};
+
+document.addEventListener("DOMContentLoaded", typewriter);
